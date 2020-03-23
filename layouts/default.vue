2afdb953e870e35c93f23b8a6fa900887e0e387d
Pablo Andrés Dorado Suárez <hola@pablodorado.com>
2020-03-22 19:42:55 -0500

    [Layouts:Default] Add base layout

---
+++ layouts/default.vue
@@ -0,0 +1,74 @@
+<script>
+import AppHeader from '../components/header';
+import AppFooter from '../components/footer';
+
+export default {
+    components: {
+        AppHeader,
+        AppFooter
+    },
+    functional: true,
+    render (createElement) {
+        return [
+            createElement(AppHeader),
+            createElement('nuxt'),
+            createElement(AppFooter)
+        ];
+    }
+}
+</script>
+
+<style>
+@font-face {
+    font-family: 'AndaleMonoMTStd';
+    font-weight: 600;
+    src: url('~assets/fonts/AndaleMonoMTStd-Bold.otf');
+}
+
+* {
+    margin: 0;
+    padding: 0;
+}
+
+body {
+    background-color: black;
+    color: white;
+    font-family: 'AndaleMonoMTStd', monospace;
+}
+
+#__layout {
+    display: grid;
+
+    grid-template: 7em auto 7em / auto 2fr;
+
+    grid-template-areas:
+        "header header header"
+        "aside main main"
+        "footer footer footer";
+    row-gap: 0.5em;
+
+    width: 100%;
+    margin-top: 1em;
+}
+
+header, main, footer {
+    align-self: center;
+    justify-self: center;
+}
+
+header {
+    grid-area: header;
+}
+
+main {
+    grid-area: main;
+}
+
+aside {
+    grid-area: aside;
+}
+
+footer {
+    grid-area: footer;
+}
+</style>
