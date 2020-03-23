a6d4d2ce1d525eb827e61d372f5846bc7ba7afb4
Pablo Andrés Dorado Suárez <hola@pablodorado.com>
2020-03-22 20:22:25 -0500

    [Pages:_] Added 404 template

---
+++ pages/_.vue
@@ -0,0 +1,25 @@
+<template>
+    <main>
+        <img src="~/assets/img/404-not-found.svg" alt="Not found">
+        <h1>page not found</h1>
+    </main>
+</template>
+
+<style scoped>
+main {
+    display: flex;
+
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+
+    height: calc(100vh - 15em);
+}
+
+img {
+    height: 80%;
+    max-height: 300px;
+
+    margin-block-end: 2em;
+}
+</style>
